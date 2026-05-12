9d5c55d98f916b3841dd299c9d68daea4c318603
Aayush <aayushofficial2005@gmail.com>
2026-05-12 12:54:13 +0530

    3rd recursion lecture

---
+++ Recursion/summation of first N numbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int SumN(int n){
+    if (n == 0) return 0;
+    return n + SumN (n - 1);
+}
+
+int main(){
+    int N = 5;
+    cout << (SumN(N));
+    return SumN(N);
+}
