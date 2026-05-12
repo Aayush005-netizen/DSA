846615e3d2f5a4dbf1b8c315680b8f1dfd53d593
Aayush <aayushofficial2005@gmail.com>
2026-05-12 12:29:11 +0530

    Adding notes

--- Recursion/print linearly from 1 to N.cpp
+++ Recursion/print linearly from 1 to N.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Because the print statement is after the recursive call, the numbers will be printed in increasing order. If we had the print statement before the recursive call, the numbers would be printed in decreasing order.
+//It is also the reason why it is a backtracking problem, because we are going back to the previous state after the recursive call is done.
+
 void print1ton(int n){
     if (n == 0) return;
     print1ton(n-1);
--- Recursion/print name 5 times.cpp
+++ Recursion/print name 5 times.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Because the print statement is after the recursive call, the numbers will be printed in increasing order. If we had the print statement before the recursive call, the numbers would be printed in decreasing order.
+//It is also the reason why it is a backtracking problem, because we are going back to the previous state after the recursive call is done.
+
 void name5(string name, int count){
     if (count == 5) return;
     name5(name,++count);
