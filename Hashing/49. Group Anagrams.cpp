4845388bc61c231b14193795c7a0b7810be898c6
Aayush <aayushofficial2005@gmail.com>
2026-08-22 12:00:46 +0530

    lc 49

---
+++ Hashing/49. Group Anagrams.cpp
@@ -0,0 +1,55 @@
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+*/
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mpp;
+        vector<vector<string>>result;
+        for(string& s : strs){
+            string key = s;
+            sort(key.begin(), key.end());   // sorted version = anagram ki "signature"
+            mpp[key].push_back(s);       // original string ko uski group mein daalo
+        }
+        for(auto& [key,substr] : mpp){
+            result.push_back(substr);
+        }
+        return result;
+
+    }
+};
